6ef0fbc3e90f1485883ab126fee967b5e0db6ee3
RonLeapYearBoy <RonLeapYearBoy@yahoo.com>
2014-10-17 18:22:59 -0400

    goodies

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,35 +1,49 @@
-#  Listing 3.13: A test for the About page. red
-#
-#  gedit   test/controllers/static_pages_controller_test.rb
-#
-# we can see that there are two tests, one for each controller action we included on the
-# command line in Listing 3.4. Each test simply gets an action and verifies (via an assertion)
-# that the result is a success.
+
+# --------------static_pages_controller_test.rb ------------------------------------ #
+# ---        09.000-Static-Pages-Controller-Test.txt                             --- #
+# ---------------------------------------------------------------------------------- #
+# ---                                                                            --- #
+# 3.4.1 Testing titles (Red)                                                         #
+# https://www.railstutorial.org/book/static_pages#sec-testing_titles                 #
+#                                                                                    #
+# Listing 3.22: The Static Pages controller test with title tests.                   #
+# https://www.railstutorial.org/book/static_pages#code-custom_home_page              #
+#                                                                                    #
+#  gedit test/controllers/static_pages_controller_test.rb                            #
+#                                                                                    #
+# checks for the presence of a <title> tag containing the string                     #
+# "Home | Ruby on Rails Tutorial Sample App"                                         #
+# ---------------------------------------------------------------------------------- #
 
 require 'test_helper'
+
 class StaticPagesControllerTest < ActionController::TestCase
 
-# says Lets test the Home page by issuing a GET request to the home action and then making
-# sure we receive a success status code in response.
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get contact" do
-    get :about
+  test "should get contactt" do
+    get :contact
     assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
+# ---------------------------------------------------------------------------------- #
+
 
